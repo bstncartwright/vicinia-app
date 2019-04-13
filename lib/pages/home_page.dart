f89658e717186fe727338e1c7840608735dbedfd
bostonc <bostonc@gmail.com>
2019-04-13 11:42:10 -0400

    location working 2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: Center(
+        child: LocationerTest(),
+      ),
+    );
+  }
+}
+
+class LocationerTest extends StatefulWidget {
+  @override
+  _LocationerTestState createState() => _LocationerTestState();
+}
+
+class _LocationerTestState extends State<LocationerTest> {
+  String location = '';
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Text("Your location is $location"),
+          MaterialButton(
+            child: Text("Get location"),
+            onPressed: () async {
+              Position position = await Geolocator()
+                  .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+              setState(() {
+                location = "${position.longitude}, ${position.latitude}";
+              });
+            },
+          )
+        ],
+      ),
+    );
   }
 }
