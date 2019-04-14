be80421757ff701d2d3b7d1a95990c6468a6df17
TheManMitch <mitchelcharette@gmail.com>
2019-04-14 05:00:18 -0400

    yup

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -12,7 +12,7 @@ class Constants extends InheritedWidget {
   Constants({Widget child, Key key}) : super(key: key, child: child);
 
   final ViciniaRepository viciniaRepository = ViciniaRepository(
-    api: DummyViciniaApiClient(
+    api: ViciniaApiClient(
       url: 'https://vicinia.azurewebsites.net/',
       dio: Dio(),
     ),
